f8271221d56441dd6ae403942cde1e9f6f577616
Matt <mdwareing@gmail.com>
2018-02-08 15:39:28 +0000

    finished

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const useragent = require('express-useragent');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(useragent.express());
+
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const lang = req.acceptsLanguages()[0];
+  const os = req.useragent.os;
+  res.json({ 'ipaddress': ipaddress, 'language': lang, 'Software': os });
+});
 
 const port = process.env.PORT || 5000;
 
